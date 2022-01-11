6d946712e89536b8686becb740593a1d01505c3b
weslley-sc <weslleydasilvacostaa@gmail.com>
2022-01-11 15:39:45 -0300

    autenticaçao finalizada

---
+++ spring-boot-spa/src/test/java/com/okta/springbootspa/SpringBootSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.springbootspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
